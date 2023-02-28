bff31da996bb3f636f8768093fae08e42c5a1923
VitaliyZhark <vitaliy.zhark@gmail.com>
2023-02-28 16:13:13 +0000

    update script
    update script

--- scrypt.js
+++ scrypt.js
@@ -6,3 +6,19 @@ const util = require('util');
 // Internal modules
 const api = require('./utils/api.js');
 const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// Inquirer prompts for userResponses
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username? (No @ needed)",
+        name: 'username',
+        default: 'connietran-dev',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
+    },
+    
