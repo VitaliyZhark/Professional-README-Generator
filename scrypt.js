a417649edac1144c4b652298c1de1eefe3474e4e
VitaliyZhark <vitaliy.zhark@gmail.com>
2023-02-28 16:11:49 +0000

    update scrypt
    update scrypt

--- scrypt.js
+++ scrypt.js
@@ -3,3 +3,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
+// Internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
