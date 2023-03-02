1c3901b320bd9f30825161e92899cbe1222b82bc
VitaliyZhark <vitaliy.zhark@gmail.com>
2023-03-02 14:03:23 +0000

    update
    update

--- script.js
+++ script.js
@@ -9,42 +9,50 @@ const questions = [
     name: "title",
     message: "What is the title of your project?",
   },
+
   {
     type: "input",
     name: "description",
     message: "Please provide a description of your project:",
   },
+
   {
     type: "input",
     name: "installation",
     message: "Please provide installation instructions for your project:",
   },
+
   {
     type: "input",
     name: "usage",
     message: "Please provide usage information for your project:",
   },
+
   {
     type: "list",
     name: "license",
     message: "Which license would you like to use for your project?",
     choices: ["MIT", "Apache", "GPL", "None"],
   },
+
   {
     type: "input",
     name: "contributing",
     message: "Please provide contribution guidelines for your project:",
   },
+
   {
     type: "input",
     name: "tests",
     message: "Please provide test instructions for your project:",
   },
+
   {
     type: "input",
     name: "github",
     message: "What is your GitHub username?",
   },
+
   {
     type: "input",
     name: "email",
@@ -81,6 +89,7 @@ function init() {
         console.log(`The file could not be generated because the user does not exist.`);
     })
 };
+
 
 // function call to initialize program
 init();
